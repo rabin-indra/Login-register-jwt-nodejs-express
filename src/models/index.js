9d0a9769963db5a12591b9cc187a86041c9e4634
intern 12 <intern12@swifttech.com>
2022-05-05 15:27:57 +0545

    first commit

---
+++ src/models/index.js
@@ -0,0 +1,28 @@
+const {Sequelize, DataTypes} = require("sequelize");
+const dbSequelize = new Sequelize('login','root','password',{
+    host : 'localhost',
+    dialect : 'mysql',
+    
+});
+
+dbSequelize.authenticate()
+.then(()=>{
+    console.log("Connected");
+})
+.catch((err)=>{
+    console.log('error :' +err);
+});
+
+
+const db = {};
+db.Sequelize = Sequelize;
+db.dbSequelize = dbSequelize;
+
+
+db.dbSequelize.sync({force : false})
+.then(() => {
+    console.log("Sync");
+});
+db.User = require("./user.model")(dbSequelize, DataTypes);
+
+module.exports = db;
